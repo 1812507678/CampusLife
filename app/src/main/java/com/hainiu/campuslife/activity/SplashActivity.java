3e71e76cc7c303bb72ca7de5c3995ebef08149a9
haijun <1812507678@qq.com>
2016-05-04 17:01:03 +0800

    5.4下午5点，添加有米广告。马海军 5.4中午，完成生活日历功能模块，基本完成全部功能，马海军 5.3下午6点，完成随手记功能模块，马海军 5.3下午4点，完成金库功能模块，马海军 5.2晚上12点，完成相册功能模块，马海军 5.1晚上12点，完成相册显示与添加分类，马海军 5.1下午，完成登陆注册，马海军

---
+++ app/src/main/java/com/hainiu/campuslife/activity/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.hainiu.campuslife.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.hainu.campuslife.R;
+
+import net.youmi.android.spot.SpotManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                        finish();
+                    }
+                });
+            }
+        }.start();
+
+        SpotManager.getInstance(this).loadSpotAds();
+        SpotManager.getInstance(this).setSpotOrientation(SpotManager.ORIENTATION_PORTRAIT);
+        SpotManager.getInstance(this).setAnimationType(SpotManager.ANIM_SIMPLE);
+        SpotManager.getInstance(this).showSpotAds(this);
+
+
+    }
+}
--- app/src/main/java/com/hainiu/campuslife/application/ApplicationInfo.java
+++ app/src/main/java/com/hainiu/campuslife/application/ApplicationInfo.java
@@ -8,6 +8,9 @@ import android.util.Log;
 
 import com.hainiu.campuslife.bean.Student;
 
+import net.youmi.android.AdManager;
+import net.youmi.android.spot.SpotManager;
+
 import cn.bmob.v3.Bmob;
 
 /**
@@ -30,6 +33,13 @@ public class ApplicationInfo extends Application{
 
         // 初始化 Bmob SDK
         Bmob.initialize(this, "d8ce0167b5dcba386a96649b4d9ad0d6");
+
+        //初始化有米广告
+        String appId= "4c14d04b6f8362e9";
+        String appSecret = "ab42475b0fbf0120";
+        AdManager.getInstance(this).init(appId, appSecret, false);
+
+
     }
 
     //初始化用户数据
